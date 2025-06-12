a78767b62581781c45e1417bd04c272a2332616a
Stijn Klomp <sklomp@friendmts.com>
2025-06-12 09:26:38 +0100

    Fish - Add env vars to config

--- fish/config.fish
+++ fish/config.fish
@@ -16,6 +16,60 @@ abbr --add py python3
 abbr --add epp export PYTHONPATH=`pwd`
 abbr --add sva source venv/bin/activate.fish
 
-#loadenv ~/.keys.env
 
+# Env variables
+# Token variables
+
+# System variables
+set -xg APP_ENV dev
+
+# NVM
+set -xg NVM_DIR "$HOME/.nvm"
+
+
+# Path
 fish_add_path ~/bin
+
+# Alises
+# Basic aliases
+alias la "ls -la"
+alias vim "nvim"
+alias kc "kubectl"
+alias tf "terraform"
+
+# AWS aliases
+alias awslogin "aws sso login --profile "
+alias awslocal "aws --endpoint-url=http://localhost:4566"
+
+# Docker compose setup
+set -g DOCKER_COMPOSE "docker-compose"
+alias dcps "$DOCKER_COMPOSE ps"
+alias dcdevdownup "$DOCKER_COMPOSE --profile dev down && $DOCKER_COMPOSE --profile dev up --build -d"
+alias dcdevupdown "$DOCKER_COMPOSE --profile dev down && $DOCKER_COMPOSE --profile dev up --build -d"
+alias dcdevdown "$DOCKER_COMPOSE --profile dev down"
+alias dcdevup "$DOCKER_COMPOSE --profile dev up --build -d"
+alias dclocaldownup "$DOCKER_COMPOSE --profile local down && $DOCKER_COMPOSE --profile local up --build -d"
+alias dclocalupdown "$DOCKER_COMPOSE --profile local down && $DOCKER_COMPOSE --profile local up --build -d"
+alias dclocaldown "$DOCKER_COMPOSE --profile local down"
+alias dclocalup "$DOCKER_COMPOSE --profile local up --build -d"
+
+# Git aliases
+alias gita "git add -A"
+alias gitc "git commit -m"
+alias gitm "git merge"
+function gitr
+    if test -z "$argv[1]"
+        echo "Usage: gitr <number>"
+    else
+        git rebase -ir HEAD~$argv[1]
+    end
+end
+alias gitac "gita && gitc"
+alias gitd "git diff"
+alias gits "git status"
+alias gitp "git pull --all"
+alias gitds "git diff --staged"
+alias gitrb "git branch -r --no-merged"
+alias gitlb "git branch --no-merged"
+alias gitbm "git merge -X theirs"
+alias gitbr "git rebase -X ours"
